8317ffa42ee609358455ab5ab098dbf3a5bf5924
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-16 16:07:38 +0100

    Removed unnecessary comments

--- controllers/maintenance/index.js
+++ controllers/maintenance/index.js
@@ -4,16 +4,15 @@ const matchesHandler = require('./matchesHandler');
 
 async function updateServer(req, res) {
     try {
-        console.warn('starting Competitions...');
         await competitionHandler();
-        console.warn('starting Teams...');
-        await teamHandler();
         console.warn('starting Matches...');
         await matchesHandler();
+        console.warn('starting Teams...');
+        await teamHandler();
         console.log('success!')
         return res.status(200).json({ message: 'Server Updated!' });
     } catch (error) {
-        console.error(error.message);
+        console.error(error);
         return res.status(500).json({ message: error.message })
     }
 };
