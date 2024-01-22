12cfac6f5ac5c049b264befb162d3cd1ec04683a
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-22 20:56:54 +0100

    Changed request method for maintenance route

--- src/routes/maintenance.js
+++ src/routes/maintenance.js
@@ -1,6 +1,6 @@
 const router = (require('express')).Router();
 const updateServer = require('../controllers/maintenance');
 
-router.post('/update', updateServer);
+router.put('/update', updateServer);
 
 module.exports = router;
