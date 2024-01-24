c9137d5a1f7d2dc8320047fd45e219b71860a27f
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-24 15:52:33 +0100

    Fixed update functions and refactored matches delete functions

--- src/controllers/maintenance/deleteRedundantMatches.js
+++ src/controllers/maintenance/deleteRedundantMatches.js
@@ -1,21 +1,23 @@
 const Match = require('../../models/Match');
 const H2H = require('../../models/H2H');
 const Team = require('../../models/Team');
-const { reduceToObjectWithIdAsKey, reduceToArrayOfMatchIds, reduceToH2HDetails, reduceToMatchDetails } = require('../../helpers/reduce');
+const { reduceToObjectWithIdAsKey, reduceToH2HDetails, reduceToMatchDetails, reduceToArrayOfMatchIds } = require('../../helpers/reduce');
 const { getDateFrom } = require("../../helpers/getDate");
         
-const getTeams = (homeTeamId, teamId) => homeTeamId === teamId ? { main: 'home', other: 'away' } : { main: 'away', other: 'home' };
-const getKeysToUpdate = ({ home, away }) => home > away ? ['wins', 'losses'] : home < away ? ['wins', 'losses'] : ['draws', 'draws'];
+const assignMainAndOtherTeam = (homeTeamId, teamId) => homeTeamId === teamId ? { main: 'home', other: 'away' } : { main: 'away', other: 'home' };
 
 const deleteRedundantMatches = () => new Promise(
     async function (resolve, reject) { 
         try {
-            const expiredMatches = await getExpiredMatches();
-            const { teamMatchIds, h2hMatchIds } = await getAllH2HAndPreviousMatchIds(expiredMatches);
-            const irrelevantMatches = await checkIrrelevantMatches({ teamMatchIds, h2hMatchIds, matches: expiredMatches });
-            await Promise.all(irrelevantMatches.map(deleteH2HAndMatches));
-            await deleteMatches();
+            await deleteIrrelevantH2HMatches();
             await deleteIrrelevantPreviousMatches();
+            await checkUpdateAndDeleteMatchesAndH2H();
+
+            console.warn("Updating Head to Heads");
+            await updateHeadToHeadMatches();
+
+            console.warn("Updating Team previous matches");
+            await updateTeamPreviousMatches();
             resolve();
         } catch (error) {
             reject(error);
@@ -23,16 +25,34 @@ const deleteRedundantMatches = () => new Promise(
     }
 );
 
+async function checkUpdateAndDeleteMatchesAndH2H() {
+    const expiredMatches = await getExpiredMatches();
+    const { teamMatchIds, h2hMatchIds } = await getAllH2HAndPreviousMatchIds(expiredMatches);
+    const irrelevantMatches = await checkIrrelevantMatches({ teamMatchIds, h2hMatchIds, matches: expiredMatches });
+    const matchesToDelete = Promise.all(irrelevantMatches.map(deleteH2HAndMatches));
+    return matchesToDelete;
+};
+
 const getExpiredMatches = () => Match.find({ isMain: true, utcDate: { $lt: getDateFrom() } }).lean();
 
 async function getAllH2HAndPreviousMatchIds(matches) {
-    const matchesH2H = matches.map(({ h2h }) => h2h);
-    const homeTeamIds = matches.map(({ homeTeam }) => homeTeam);
-    const awayTeamIds = matches.map(({ awayTeam }) => awayTeam);
-    const h2hs = await H2H.find({ _id: { $not: { $in: matchesH2H } } }).lean();
-    const teams = await Team.find({ $or: [{ _id: { $not: { $in: homeTeamIds } } }, { _id: { $not: { $in: awayTeamIds } } }] }).lean();
-    const teamMatchIds = teams.reduce(reduceToArrayOfMatchIds, []);
-    const h2hMatchIds = h2hs.reduce(reduceToArrayOfMatchIds, []);
+    const teamIds = matches.reduce((homeAndAwayTeamIds, { homeTeam, awayTeam }) => [...homeAndAwayTeamIds, homeTeam, awayTeam], []);
+    const h2hMatches = await Match.find({
+        $and: [
+            { isHead2Head: true },
+            { homeTeam: { $not: { $in: teamIds } } },
+            { awayTeam: { $not: { $in: teamIds } } }
+        ]
+    }).lean();
+    const teamMatches = await Match.find({
+        $and: [
+            { isPrevMatch: true },
+            { homeTeam: { $not: { $in: teamIds } } },
+            { awayTeam: { $not: { $in: teamIds } } }
+        ]
+    }).lean();
+    const teamMatchIds = teamMatches.map(({ _id }) => _id);
+    const h2hMatchIds = h2hMatches.map(({ _id}) => _id);
     return { teamMatchIds, h2hMatchIds };
 };
 
@@ -42,59 +62,91 @@ async function checkIrrelevantMatches({ teamMatchIds, h2hMatchIds, matches }) {
         const matchId = match._id;
         const isATeamMatch = teamMatchIds.includes(matchId);
         const isAH2HMatch = h2hMatchIds.includes(matchId);
-        if (isATeamMatch) await updateTeamPreviousMatches(matchId);
-        else if (isAH2HMatch) await updateHeadToHeadMatches(matchId);
+        if (isATeamMatch || isAH2HMatch) continue;
         else irrelevantMatches.push(match);
     }
     return irrelevantMatches;
 };
 
 async function deleteH2HAndMatches(irrelevantMatch) {
-    const h2h = await Match.findOne({ _id: { "$ne": irrelevantMatch._id }, h2h: irrelevantMatch.h2h, isMain: true });
+    const h2h = await Match.findOne({ _id: { $ne: irrelevantMatch._id }, h2h: irrelevantMatch.h2h, isMain: true });
     if (!h2h) return;
     await H2H.deleteMany({ _id: irrelevantMatch.h2h });
 };
 
-async function deleteMatches() {
-    const teams = await Team.find().lean();
+async function deleteIrrelevantH2HMatches() {
     const h2hs = await H2H.find().lean();
-    const teamMatches = teams.reduce(reduceToArrayOfMatchIds, []);
-    const h2hMatches = h2hs.reduce(reduceToArrayOfMatchIds, []);
-    const allRelevantMatches = [...teamMatches, ...h2hMatches];
-    return Match.deleteMany({ _id: { $not: { $in: allRelevantMatches } } });
+
+    const h2hMatchIds = h2hs.reduce(reduceToArrayOfMatchIds, []);
+
+    return Match.deleteMany({
+        _id: { $not: { $in: h2hMatchIds } },
+        isPrevMatch: { $ne: true },
+        isHead2Head: true
+    });
 };
 
-async function updateTeamPreviousMatches(matchId) {
-    const teams = await Team.find({ matches: { $in: matchId } });
+async function updateTeamPreviousMatches() {
+    const teams = await Team.find();
+    const matchesToUpdate = [];
     for (let team of teams) {
-        const matches = await Match.find({ _id: { $in: teams._doc.matches }, status: 'FINISHED' }).lean();
-        const teamMatches = matches.map(match => ({ ...match, teams: getTeams(match.homeTeam, team._doc._id) }));
+        const matches = await Match.find({
+            $or: [{ homeTeam: team._doc._id }, { awayTeam: team._doc._id }],
+            status: 'FINISHED'
+        }).lean();
+
+        const teamMatches = matches.map(match => ({ ...match, teams: assignMainAndOtherTeam(match.homeTeam, team._doc._id) }));
         const { matchesPlayed, wins, draws, losses } = teamMatches.reduce(reduceToMatchDetails, { matchesPlayed: 0, wins: 0, draws: 0, losses: 0 });
+
         team.matchesPlayed = matchesPlayed;
         team.wins = wins;
         team.draws = draws;
         team.losses = losses;
+
+        const matchIds = matches.map(match => match._id);
+        matchesToUpdate.push(...matchIds);
         await team.save();
-    }
+    };
+
+    return Match.updateMany({ _id: { $in: matchesToUpdate } }, { $set: { isPrevMatch: true } });
 };
 
-async function updateHeadToHeadMatches(matchId) {
-    const h2h = await H2H.findOne({ matches: { $in: matchId } });
-    const matches = await Match.find({ _id: { $in: h2h._doc.matches }, status: 'FINISHED' });
-    const initialValues = { numberOfMatches: 0, totalGoals: 0, homeTeam: 0, awayTeam: 0 };
-    const { numberOfMatches, totalGoals, homeTeam, awayTeam } = matches.reduce(reduceToH2HDetails, initialValues);
-    h2h.numberOfMatches = numberOfMatches;
-    h2h.totalGoals = totalGoals;
-    h2h.homeTeam = homeTeam;
-    h2h.awayTeam = awayTeam;
-    await h2h.save();
+async function updateHeadToHeadMatches() {
+    const matchesToUpdate = [];
+    const headToHeads = await H2H.find().lean();
+
+    for (const headToHead of headToHeads) {
+        const matches = await Match.find({
+            $or: [
+                { homeTeam: headToHead.aggregates.homeTeam.id, awayTeam: headToHead.aggregates.awayTeam.id },
+                { awayTeam: headToHead.aggregates.homeTeam.id, homeTeam: headToHead.aggregates.awayTeam.id }
+            ],
+            status: 'FINISHED'
+        }).lean();
+
+        const initialValues = { numberOfMatches: 0, totalGoals: 0, homeTeam: 0, awayTeam: 0 };
+        const { numberOfMatches, totalGoals, homeTeam, awayTeam } = matches.reduce(reduceToH2HDetails, initialValues);
+
+        headToHead.numberOfMatches = numberOfMatches;
+        headToHead.totalGoals = totalGoals;
+        headToHead.homeTeam = homeTeam;
+        headToHead.awayTeam = awayTeam;
+
+        const matchIds = matches.map(match => match._id);
+        matchesToUpdate.push(...matchIds);
+
+        await H2H.findByIdAndUpdate(headToHead._id, { $set: { matches: matchIds } });
+    }
+
+    return Match.updateMany({ _id: { $in: matchesToUpdate } }, { $set: { isHead2Head: true }});
 };
 
 async function deleteIrrelevantPreviousMatches() {
     const teams = await Team.find({}, { _id: 1 }).lean();
     const matchIdsInArray = await Promise.all(teams.map(getTeamMatches));
+
     const matchesWithTheirPositionForEachTeam = matchIdsInArray.reduce(reduceToObjectWithIdAsKey, {});
-    console.log(matchesWithTheirPositionForEachTeam);
+
     for (let [matchId, matchPositions] of Object.entries(matchesWithTheirPositionForEachTeam)) {
         if (matchPositions.some(position => position < 4)) continue;
         await Match.deleteOne({ _id: matchId });
@@ -112,6 +164,7 @@ async function getTeamMatches (team) {
             ]
         }
     ).sort({ utcDate: -1 }).lean();
+
     const prevMatchesIds = teamPrevMatches.map(match => match._id);
     return prevMatchesIds;
 }
