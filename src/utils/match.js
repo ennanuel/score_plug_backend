e19807d6803fe643930d5a744b375b6f24614abf
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-24 03:54:39 +0100

    Refactored functions and added new functions to export

--- src/utils/match.js
+++ src/utils/match.js
@@ -1,9 +1,11 @@
 const H2H = require('../models/H2H');
+const Team = require("../models/Team");
+const Competition = require('../models/Competition');
+const Match = require('../models/Match');
 
 const { VALID_MATCH_STATUS_REGEX } = require("../constants");
 
 const { getDateFrom } = require("../helpers/getDate");
-const { reduceToObjectWithIdAsKey } = require('../helpers/reduce');
 
 
 function updateMatchStatusAndScore({ previousMatches, currentMatches }) {
@@ -21,47 +23,89 @@ function updateMatchStatusAndScore({ previousMatches, currentMatches }) {
 function createMatchFilterRegExp(filter) {
     const filterIsInPlay = filter?.toLowerCase() === 'in_play';
     const filterValue = filterIsInPlay ? 'in_play|paused' : filter;
-    const noFilter = '[a-z]'
+    const noFilter = '[a-z]';
     const regExp = new RegExp(VALID_MATCH_STATUS_REGEX.test(filter) ? filterValue : noFilter, 'i');
     return regExp;
 };
 
-async function getMatchesHead2Head(matches) {
-    if (!matches) return []
-    const matchesHead2HeadIds = matches.map(match => match.head2head);
-    const H2Hs = await H2H.find({ _id: { $in: matchesHead2HeadIds } });
-    return H2Hs;
+async function getMatchTeams(match) {
+    const matchTeamIds = [match.homeTeam, match.awayTeam];
+    const teams = await Team.find({ _id: { $in: matchTeamIds } }, '_id name shortName tla wins losses draws crest').lean();
+    const [homeTeam, awayTeam] = teams.sort(team => team._id === match.homeTeam ? -1 : 1);
+    return { homeTeam, awayTeam };
 };
 
-const expandAllPreviousMatches = (previousMatches, head2head) => [...previousMatches, ...head2head.aggregates.homeTeam.previousMatches, ...head2head.aggregates.awayTeam.previousMatches];
+const getCompetition = (competitionId) => Competition.findById(competitionId, '_id name area type emblem').lean();
 
-const expandH2HMatches = (head2headMatches, head2head) => [...head2headMatches, ...head2head.matches];
+async function expandMatchTeamsAndCompetition(match) {
+    const competition = await getCompetition(match.competition);
+    const { homeTeam, awayTeam } = await getMatchTeams(match);
+    const expandedMatch = { ...match, homeTeam, awayTeam, competition };
+    return expandedMatch;
+};
 
-function joinH2HandMatches({ head2headMatches, previousMatches, matchesHeadToHeads }) {
-    const reducedH2HMatches = head2headMatches.reduce(reduceToObjectWithIdAsKey, {});
-    const reducedPreviousMatches = previousMatches.reduce(reduceToObjectWithIdAsKey, {});
-    const joinedH2Hs = [];
-    for (let headToHead of matchesHeadToHeads) {
-        const newH2H = { ...headToHead };
-        const homeTeamPreviousMatches = headToHead.aggregates.homeTeam.previousMatches.map(match => reducedPreviousMatches[match]);
-        const awayTeamPreviousMatches = headToHead.aggregates.awayTeam.previousMatches.map(match => reducedPreviousMatches[match]);
-        const h2hMatches = headToHead.matches.map(match => reducedH2HMatches[match]);
-        newH2H.aggregates.homeTeam.previousMatches = homeTeamPreviousMatches;
-        newH2H.aggregates.awayTeam.previousMatches = awayTeamPreviousMatches;
-        newH2H.matches = h2hMatches;
-        joinedH2Hs.push(newH2H);
+async function getMatchHead2Head(h2hId) {
+    const head2head = await H2H.findById(h2hId).lean();
+    const head2headMatches = await Match.find({ _id: { $in: head2head.matches } }).lean();
+    head2head.matches = head2headMatches;
+    return head2head;
+};
+
+function arrangeHead2HeadTeams({ head2head, homeTeamId, awayTeamId }) {
+    const arrangedHead2Head = { ...head2head };
+    const { homeTeam, awayTeam } = head2head.aggregates;
+
+    if (homeTeam.id === awayTeamId && awayTeam.id === homeTeamId) {
+        arrangedHead2Head.homeTeam = awayTeam;
+        arrangedHead2Head.awayTeam = homeTeam;
     }
-    return joinedH2Hs;
+
+    return arrangedHead2Head;
+}
+
+async function getMatchHead2HeadAndPreviousMatches(match) {
+    const matchHead2Head = await getMatchHead2Head(match.head2head);
+    const arrangedHead2Head = arrangeHead2HeadTeams({ head2head: matchHead2Head, homeTeamId: match.homeTeam._id, awayTeamId: match.awayTeam._id });
+
+    const homeTeamPreviousMatches = await Match.find({
+        isPreviousMatch: true,
+        $or: [{ homeTeam: match.homeTeam._id, awayTeam: match.homeTeam._id }],
+    }).lean();
+    const awayTeamPreviousMatches = await Match.find({
+        isPreviousMatch: true,
+        $or: [{ homeTeam: match.awayTeam._id, awayTeam: match.awayTeam._id }]
+    }).lean();
+
+    const result = { ...match, head2head: arrangedHead2Head };
+    
+    result.homeTeam.previousMatches = homeTeamPreviousMatches;
+    result.awayTeam.previousMatches = awayTeamPreviousMatches;
+
+    return result;
 };
+
+const calculateOutcomePercentage = ({ team1, team2, total }, [key1, key2]) => (((((team1.h2h[key1] + team2.h2h[key2]) * 2) + team1.prevMatches[key1] + team2.prevMatches[key2]) * 100) / total).toFixed(2);
+
+function getMatchOutcome(match) {
+    const totalMatchesPlayed = (match.head2head.aggregates.numberOfMatches * 4) + match.homeTeam.matchesPlayed + match.awayTeam.matchesPlayed;
+    const homeTeam = { h2h: match.head2head.aggregates.homeTeam, prevMatches: match.homeTeam };
+    const awayTeam = { h2h: match.head2head.aggregates.awayTeam, prevMatches: match.awayTeam };
+    const homeWinOutcome = calculateOutcomePercentage({ team1: homeTeam, team2: awayTeam, total: totalMatchesPlayed }, ['wins', 'losses']);
+    const drawOutcome = calculateOutcomePercentage({ team1: homeTeam, team2: awayTeam, total: totalMatchesPlayed }, ['draws', 'draws']);
+    const awayWinOutcome = calculateOutcomePercentage({ team1: awayTeam, team2: homeTeam, total: totalMatchesPlayed }, ['wins', 'losses'])
+    const outcome = { homeWin: +homeWinOutcome, draw: +drawOutcome, awayWin: +awayWinOutcome };
+    const matchWithOutcome = { ...match, outcome };
+    return matchWithOutcome;
+}
 
 const checkIfIsMainMatch = (matchDate) => (new Date(matchDate)).getTime() >= (new Date(getDateFrom())).getTime();
 
 module.exports = {
     updateMatchStatusAndScore,
     createMatchFilterRegExp,
-    getMatchesHead2Head,
-    expandAllPreviousMatches,
-    expandH2HMatches,
-    joinH2HandMatches,
-    checkIfIsMainMatch
+    checkIfIsMainMatch,
+    getMatchTeams,
+    expandMatchTeamsAndCompetition,
+    getMatchHead2HeadAndPreviousMatches,
+    getMatchOutcome
 }
