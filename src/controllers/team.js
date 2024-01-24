04cc8bad0fc1723abce95845616456f6a6864fb1
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-24 15:55:43 +0100

    Added functionality to the controller functions

--- src/controllers/team.js
+++ src/controllers/team.js
@@ -1,8 +1,22 @@
+const Team = require("../models/Team");
 
+async function getAllTeams(req, res) { 
+    try {
+        return res.status(200).json({ data: "nothing yet" });
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ message: error.message })
+    }
+};
 
-async function getAllTeams(req, res) { };
-
-async function getTeamDetails(req, res) { };
+async function getTeamDetails(req, res) { 
+    try {
+        return res.status(200).json({ data: "nothing yet" });
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ message: error.message });
+    }
+};
 
 module.exports = {
     getAllTeams,
