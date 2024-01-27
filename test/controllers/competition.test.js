82b10e1cea54b34633623e5dce427b9f5cffb199
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-27 17:17:20 +0100

    The test file that contains all tests for the competition route

---
+++ test/controllers/competition.test.js
@@ -0,0 +1,31 @@
+const Team = require("../../src/models/Team");
+const Match = require("../../src/models/Match");
+const Competition = require("../../src/models/Competition");
+
+jest.mock("../src/models/Team");
+jest.mock("../src/models/Match");
+jest.mock("../src/models/Competition");
+
+describe("", () => {
+
+});
+
+describe("", () => {
+
+});
+
+describe("", () => {
+
+});
+
+describe("", () => {
+
+});
+
+describe("", () => {
+
+});
+
+describe("", () => { 
+    
+});
