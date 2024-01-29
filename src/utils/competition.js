fb0898c7d770ec7b903d867a96790559cad02af0
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-29 20:47:27 +0100

    Added and exported new utility functions

--- src/utils/competition.js
+++ src/utils/competition.js
@@ -1,11 +1,27 @@
+const { reduceToObjectWithIdAsKeys } = require("../helpers/reduce");
+const Team = require("../models/Team");
+const { getMatchWithTeamData } = require("./match");
 
-
-const reduceToActiveCompetitionsIds = (competitionIds, match) => {
-    const competitionId = match.competition;
-    if (competitionIds.includes(competitionId)) return competitionIds;
-    return [...competitionIds, competitionId]
+async function expandCompetitionsMatches({ competitions, matches }) {
+    const updatedMatches = matches.map(getMatchWithTeamData);
+    const matchesWithTeamData = await Promise.all(updatedMatches);
+    const competitionsWithExpandedMatches = competitions.map(competition => ({
+        ...competition,
+        matches: matchesWithTeamData.filter(match => match.competition === competition._id)
+    }));
+    return competitionsWithExpandedMatches;
 };
 
+async function expandTableTeams(standing) {
+    const teamIds = standing.table.map(({ team }) => team);
+    const teams = await Team.find({ _id: { $in: teamIds } }).lean();
+    const teamsObjectWithIdAsKey = teams.reduce(reduceToObjectWithIdAsKeys, {});
+    const expandedTable = standing.table.map(position => ({ ...position, team: teamsObjectWithIdAsKey[position.team] }));
+    const newStanding = { ...standing, table: expandedTable };
+    return newStanding;
+}
+
 module.exports = { 
-    reduceToActiveCompetitionsIds
+    expandCompetitionsMatches,
+    expandTableTeams
 }
