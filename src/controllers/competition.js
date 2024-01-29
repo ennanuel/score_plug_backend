19a289e1e61da726d8b669ad1d42e6a066d12767
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-01-29 20:42:42 +0100

    Refactored functions and added pagination logic

--- src/controllers/competition.js
+++ src/controllers/competition.js
@@ -1,9 +1,14 @@
-const { getFromToDates } = require("../helpers/getDate");
 const Competition = require("../models/Competition");
 const Match = require("../models/Match");
-const { reduceToActiveCompetitionsIds } = require("../utils/competition");
 
-async function getAllCompetitions(req, res) {
+const { expandCompetitionsMatches, expandTableTeams } = require("../utils/competition");
+
+const { getFromToDates } = require("../helpers/getDate");
+const { reduceToMatchCompetitionsIds } = require("../helpers/reduce");
+const { getMatchWithTeamData } = require("../utils/match");
+const Team = require("../models/Team");
+
+async function getCompetitions(req, res) {
     try {
         const competitions = await Competition.find().lean();
         return res.status(200).json(competitions);
@@ -13,44 +18,19 @@ async function getAllCompetitions(req, res) {
     }
 };
 
-async function getCompetitionsWithMatches(req, res) { 
-    try {
-        return res.status(200).json([])
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: error.message });
-    }
-};
-
-async function getCompetitionMatches(req, res) {
-    try {
-        return res.status(200).json([])
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: error.message });
-    }
-};
-
-async function getCompetitionTeams(req, res) { 
-    try {
-        return res.status(200).json([])
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: error.message });
-    }
-};
-
 async function getActiveCompetitions(req, res) {
     try {
         const { from, to } = req.query;
         const { dateFrom, dateTo } = getFromToDates(from, to);
 
-        const activeMatches = await Match.find({ utcDate: { gte: dateFrom, lt: dateTo } }, '_id').lean();
-        const activeCompetitionIds = activeMatches.reduce(reduceToActiveCompetitionsIds, []);
+        const activeMatches = await Match.find({ utcDate: { gte: dateFrom, lt: dateTo } }).lean();
+        const activeCompetitionIds = activeMatches.reduce(reduceToMatchCompetitionsIds, []);
 
-        const activeCompetitions = await Competition.find({ _id: { $in: activeCompetitionIds } });
+        const activeCompetitions = await Competition.find({ _id: { $in: activeCompetitionIds } }).lean();
 
-        return res.status(200).json(activeCompetitions);
+        const expandedCompetitions = await expandCompetitionsMatches({ competitions: activeCompetitions, matches: activeMatches });
+
+        return res.status(200).json(expandedCompetitions);
     } catch (error) {
         console.error(error.message);
         return res.status(500).json({ message: error.message });
@@ -69,11 +49,58 @@ async function getCompetitionDetails(req, res) {
     }
 };
 
+async function getCompetitionStandings(req, res) {
+    try {
+        const { competitionId } = req.params;
+        const competition = await Competition.findById(competitionId).lean();
+        if (!competition) throw new Error('Could not find competition');
+        const standings = competition.standings;
+        const standingsToExpand = standings.map(expandTableTeams);
+        const expandedStandings = await Promise.all(standingsToExpand);
+        return res.status(200).json(expandedStandings);
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+async function getCompetitionMatches(req, res) {
+    try {
+        const { competitionId } = req.params;
+        const { from, to } = req.query;
+        const { dateFrom, dateTo } = getFromToDates(from, to);
+
+        const competitionMatches = await Match.find({ utcDate: { gte: dateFrom, lt: dateTo }, competition: competitionId }).lean();
+        
+        const matchesToExpand = competitionMatches.map(getMatchWithTeamData);
+        const expandedMatches = await Promise.all(matchesToExpand);
+
+        return res.status(200).json(expandedMatches);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+async function getCompetitionTeams(req, res) { 
+    try {
+        const { competitionId } = req.params;
+        const competition = await Competition.findById(competitionId).lean();
+        if (!competition) throw new Error("No competitions found");
+        
+        const competitionTeams = await Team.find({ _id: { $in: competition.teams } }).lean();
+        return res.status(200).json(competitionTeams);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: error.message });
+    }
+};
+
 module.exports = {
-    getAllCompetitions,
-    getCompetitionsWithMatches,
+    getCompetitions,
     getCompetitionMatches,
     getCompetitionTeams,
     getActiveCompetitions,
-    getCompetitionDetails
+    getCompetitionDetails,
+    getCompetitionStandings
 }
