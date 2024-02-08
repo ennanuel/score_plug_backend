79b42482a85e19eee00590a8993a8005963fef41
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-02-08 20:46:33 +0100

    Added new route for updating matches

--- src/routes/maintenance.js
+++ src/routes/maintenance.js
@@ -1,6 +1,9 @@
 const router = (require('express')).Router();
-const updateServer = require('../controllers/maintenance');
+const updateServer = require('../controllers/maintenance/updateServer/');
+const updateMatches = require('../controllers/maintenance/updateMatches');
 
 router.put('/update', updateServer);
+
+router.put("/matches", updateMatches);
 
 module.exports = router;
