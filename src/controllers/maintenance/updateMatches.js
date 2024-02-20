dde6033629af526f044854f94c683e26950df65c
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-02-20 10:56:10 +0100

    Refactored code

--- src/controllers/maintenance/updateMatches.js
+++ src/controllers/maintenance/updateMatches.js
@@ -1,23 +1,27 @@
 const { fetchHandler } = require("../../helpers/fetchHandler");
 const { reduceToObjectWithIdAsKeys } = require("../../helpers/reduce");
 const Match = require("../../models/Match");
+const { changeMatchScoreFormat } = require("../../utils/match");
 const { getTimeForNextUpdateCall, updateSchedules } = require("../../utils/scheduler");
 
 async function updateMatches(req, res) {
     try {
         const { matches } = await fetchHandler(`${process.env.FOOTBALL_API_URL}/matches`);
         const matchIds = matches.map(match => match.id);
-        const matchesObjectWithIdAsKey = matches.reduce(reduceToObjectWithIdAsKeys);
+        const matchesObjectWithIdAsKey = matches.reduce(reduceToObjectWithIdAsKeys, {});
         const matchesInDB = await Match.find({ _id: { $in: matchIds } });
-        const matchesToSave = []
+        const matchesToSave = [];
+        let matchesUpdated = 0;
 
         for (const match of matchesInDB) {
             const matchWithUpdatedValue = matchesObjectWithIdAsKey[match._id];
-            if (matchWithUpdatedValue.lastUpdated === match.lastUpdated) continue;
+
+            if (matchWithUpdatedValue.lastUpdated === match._doc.lastUpdated) continue;
             match.status = matchWithUpdatedValue.status;
-            match.score = matchWithUpdatedValue.score;
+            match.score = changeMatchScoreFormat(matchWithUpdatedValue.score);
             match.lastUpdated = matchWithUpdatedValue.lastUpdated;
             matchesToSave.push(match.save());
+            matchesUpdated++;
         };
 
         await Promise.all(matchesToSave);
@@ -25,7 +29,7 @@ async function updateMatches(req, res) {
         if (failed) throw new Error(message);
 
         const nextUpdateCallTime = getTimeForNextUpdateCall();
-        return res.status(200).json({ message: `matches updated!`, nextCall: nextUpdateCallTime });
+        return res.status(200).json({ message: `${matchesUpdated} matches updated!`, nextCall: nextUpdateCallTime });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: error.message });
