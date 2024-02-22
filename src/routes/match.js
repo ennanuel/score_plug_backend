1d52eefc0111cc9f5bcfe066270165658eea8ad5
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-02-22 08:20:51 +0100

    Removed handling for '/update' route

--- src/routes/match.js
+++ src/routes/match.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-const { updateMatches, getAllMatches, getMatchPicks, getMatchDetails } = require('../controllers/match');
+const { getAllMatches, getMatchPicks, getMatchDetails } = require('../controllers/match');
 
 router.get('/:matchId', getMatchDetails);
 router.get('/', getAllMatches);
 router.get('/prediction/outcomes', getMatchPicks);
-router.put('/update', updateMatches);
 
 module.exports = router
