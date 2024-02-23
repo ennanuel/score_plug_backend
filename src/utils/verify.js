7d9da5dc47a07aed3ebd5a767194ccb753d0af9c
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-02-23 10:42:09 +0100

    Added function to verity authentication token

---
+++ src/utils/verify.js
@@ -0,0 +1,17 @@
+function verifyAuthToken(req, res, next) {
+    try {
+        const headers = req.headers;
+        const authToken = headers['auth-token'];
+
+        if (authToken !== process.env.AUTH_TOKEN) throw new Error("You are not authorized!");
+        
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(403).json({ message: error.message });
+    }
+}
+
+module.exports = {
+    verifyAuthToken
+}
