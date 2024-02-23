41a8f8a915ff0d13be92ef96caa6affece4b1c5f
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-02-23 22:55:51 +0100

    Removed log of headers

--- src/utils/verify.js
+++ src/utils/verify.js
@@ -2,7 +2,6 @@ function verifyAuthToken(req, res, next) {
     try {
         const headers = req.headers;
         const authToken = headers['auth-token'];
-        console.log(headers);
 
         if (authToken !== process.env.AUTH_TOKEN) throw new Error("You are not authorized!");
         
