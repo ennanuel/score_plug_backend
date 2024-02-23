24a8e189a0b17ce52bf2abce785141b8549489e0
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-02-23 10:42:42 +0100

    Implemented auth verification before request can be handled

--- src/routes/maintenance.js
+++ src/routes/maintenance.js
@@ -1,9 +1,10 @@
 const router = (require('express')).Router();
 const updateServer = require('../controllers/maintenance/updateServer/');
 const updateMatches = require('../controllers/maintenance/updateMatches');
+const { verifyAuthToken } = require('../utils/verify');
 
-router.put('/update/server', updateServer);
+router.put('/update/server', verifyAuthToken, updateServer);
 
-router.put("/update/match", updateMatches);
+router.put("/update/match", verifyAuthToken, updateMatches);
 
 module.exports = router;
