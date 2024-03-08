8ed96a370fbf17a0fc5cb2fd2c82c878bc0b54c6
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-03-08 01:57:12 +0100

    Added /graphql route

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const cors = require('cors')
-const bp = require('body-parser')
-const mongoose = require('mongoose')
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bp = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
 
 const authRoute = require('./src/routes/auth');
 const teamRoute = require('./src/routes/team');
@@ -11,6 +11,8 @@ const playerRoute = require('./src/routes/player');
 const matchRoute = require('./src/routes/match');
 const searchRoute = require('./src/routes/search');
 const maintenanceRoute = require('./src/routes/maintenance');
+
+const schema = require('./src/controllers/schema');
 
 dotenv.config();
 
@@ -32,5 +34,10 @@ app.use('/api/v2/competition', compRoute);
 app.use('/api/v2/player', playerRoute);
 app.use('/api/v2/match', matchRoute);
 app.use('/api/v2/search', searchRoute);
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV == 'development'
+}))
 
 module.exports = app;
