9136b4dfb003385893aa445cfe627e836162401a
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-03 21:43:17 +0100

    Added new logic to handle match head-to-head and refactored code

--- src/controllers/maintenance/updateServer/matchesHandler.js
+++ src/controllers/maintenance/updateServer/matchesHandler.js
@@ -3,11 +3,12 @@ const H2H = require('../../../models/H2H');
 
 const deleteRedundantMatches = require('./deleteRedundantMatches');
 const { fetchHandler, delay } = require('../../../helpers/fetchHandler');
-const { prepareForBulkWrite, prepareMatchForUpload } = require('../../../helpers/mongoose');
+const { prepareForBulkWrite, prepareMatchForUpload, refineH2HValues } = require('../../../helpers/mongoose');
 
 const { getDateFilters } = require("../../../helpers/getDate");
 const { refineMatchValues } = require("../../../helpers/mongoose");
-const { checkIfIsMainMatch } = require("../../../utils/match")
+const { checkIfIsMainMatch } = require("../../../utils/match");
+
 
 const matchesHandler = () => new Promise(
     async function (resolve, reject) {
@@ -17,7 +18,7 @@ const matchesHandler = () => new Promise(
             await saveMainMatches(matchesToSave);
             await handleMatchesWithoutHead2Head();
 
-            console.log('Deleting irrelevant matches and calculating matches outcomes...');
+            console.warn('Deleting irrelevant matches and calculating matches outcomes...');
             await deleteRedundantMatches();
 
             resolve();
@@ -32,11 +33,12 @@ const getMatchesToSave = () => new Promise(
         try {
             const { dateFrom, dateTo } = getDateFilters();
             const URL = `${process.env.FOOTBALL_API_URL}/matches?dateFrom=${dateFrom}&dateTo=${dateTo}`;
-            const matchesResult = await fetchHandler(URL);
-            const fetchedMatches = matchesResult.matches;
+            const { matches } = await fetchHandler(URL);
+
             const savedMatches = await Match.find({ isMain: true }, '_id').lean();
             const savedMatchesIds = savedMatches.map(match => match._id);
-            const filteredMatches = fetchedMatches.filter(match => !savedMatchesIds.includes(match.id));
+            
+            const filteredMatches = matches.filter(match => !savedMatchesIds.includes(match.id));
             resolve(filteredMatches);
         } catch (error) {
             reject(error);
@@ -80,19 +82,26 @@ const prepareMatchHeadToHead = (matches) => new Promise(
         try {
             const head2heads = [];
             for (let match of matches) {
+                if (match.head2head) continue;
+                
                 console.log('preparing to update: %s', match._doc._id);
-                const H2HDataURL = `${process.env.FOOTBALL_API_URL}/matches/${match._doc._id}/head2head?limit=10`
+                const H2HDataURL = `${process.env.FOOTBALL_API_URL}/matches/${match._doc._id}/head2head?limit=10`;
                 const { resultSet, aggregates, matches } = await fetchHandler(H2HDataURL);
+                await delay();
+
                 const savedH2HMatches = await saveH2HMatches(matches);
                 savedH2HMatches.unshift(match._doc._id);
+                
                 const id = `${match._doc.homeTeam}${match._doc.awayTeam}`;
-                const head2head = prepareForBulkWrite({ _id: id, resultSet, aggregates, matches: savedH2HMatches });
+                if (!aggregates) continue;
+                const h2hToSave = refineH2HValues({ id, resultSet, aggregates, matches: savedH2HMatches });
+                const head2head = prepareForBulkWrite(h2hToSave);
+                
                 match.isMain = checkIfIsMainMatch(match._doc.utcDate);
                 match.head2head = id;
                 await match.save();
                 console.log('match updated: %s', match._doc._id);
                 head2heads.push(head2head);
-                await delay();
             }
             resolve(head2heads);
         } catch (error) {
@@ -104,14 +113,7 @@ const prepareMatchHeadToHead = (matches) => new Promise(
 const saveH2HMatches = (matches) => new Promise(
     async function (resolve, reject) {
         try {
-            const matchesToSave = matches.map(match => ({
-                ...match,
-                _id: match.id,
-                isHead2Head: true,
-                competition: match.competition.id,
-                homeTeam: match.homeTeam.id,
-                awayTeam: match.awayTeam.id,
-            }));
+            const matchesToSave = matches.map((match) => refineMatchValues({ ...match, isHead2Head: true }));
             const preparedMatches = matchesToSave.map(prepareForBulkWrite);
             await Match.bulkWrite(preparedMatches);
             const savedMatchesIds = preparedMatches.map(match => match._id);
