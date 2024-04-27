47a39b6bd4f5cad4b6616e53000add53466ad4b8
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-04-27 13:56:32 +0100

    made changes

--- app.js
+++ app.js
@@ -1,49 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const cors = require('cors');
-const bp = require('body-parser');
-const { graphqlHTTP } = require('express-graphql');
 
 const http = require('http');
 const { Server } = require('socket.io');
 
-const authRoute = require('./src/routes/auth');
-const teamRoute = require('./src/routes/team');
-const compRoute = require('./src/routes/competition');
-const playerRoute = require('./src/routes/player');
-const matchRoute = require('./src/routes/match');
-const searchRoute = require('./src/routes/search');
-const maintenanceRoute = require('./src/routes/maintenance');
-const schema = require('./src/routes/schema');
-
-dotenv.config();
-
 const app = express();
 
-app.use(
-    cors({
-        origin: process.env.FRONTEND_URL
-    })
-);
-
-app.use(bp.json());
-app.use(bp.urlencoded({ extended: true }));
-
-app.use('/images', express.static("images"));
-app.use('/maintenance', maintenanceRoute);
-app.use('/api/v2/auth', authRoute);
-app.use('/api/v2/team', teamRoute);
-app.use('/api/v2/competition', compRoute);
-app.use('/api/v2/player', playerRoute);
-app.use('/api/v2/match', matchRoute);
-app.use('/api/v2/search', searchRoute);
-
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: process.env.NODE_ENV == 'development'
-}));
-
 const server = http.createServer(app);
+
+dotenv.config();
 
 const io = new Server(server, {
     cors: {
--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 const mongoose = require("mongoose");
+const { graphqlHTTP } = require('express-graphql');
+const express = require('express');
+const cors = require('cors');
+const bp = require('body-parser');
+const dotenv = require('dotenv');
+
+const authRoute = require('./src/routes/auth');
+const teamRoute = require('./src/routes/team');
+const compRoute = require('./src/routes/competition');
+const playerRoute = require('./src/routes/player');
+const matchRoute = require('./src/routes/match');
+const searchRoute = require('./src/routes/search');
+const maintenanceRoute = require('./src/routes/maintenance');
+const schema = require('./src/routes/schema');
 
 const { app, io, server } = require('./app');
 
-// The express is declared in a different file to fix jest exit error when testing.
+dotenv.config();
+
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL
+    })
+);
+
+app.use(bp.json());
+app.use(bp.urlencoded({ extended: true }));
+
+app.use('/images', express.static("images"));
+app.use('/maintenance', maintenanceRoute);
+app.use('/api/v2/auth', authRoute);
+app.use('/api/v2/team', teamRoute);
+app.use('/api/v2/competition', compRoute);
+app.use('/api/v2/player', playerRoute);
+app.use('/api/v2/match', matchRoute);
+app.use('/api/v2/search', searchRoute);
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV == 'development'
+}));
 
 mongoose.set('strictQuery', false);
 
@@ -16,7 +53,6 @@ mongoose
 
         io.on("connection", (socket) => {
             console.log('Socket connected: %s', socket.id);
-            socket.on('group-action', (params, type) => memberActions({ ...params, type, socket, io }))
         });
     }))
     .catch((err) => console.log(err));
