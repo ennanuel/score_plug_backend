564781aa499be6eb307109189b50c542981d2d94
Eunoia <ezemaemmanuel6@gmail.com>
2024-12-27 12:28:22 +0100

    removed unnecessary routes

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ const cors = require('cors');
 const bp = require('body-parser');
 const dotenv = require('dotenv');
 
-const authRoute = require('./src/routes/auth');
-const teamRoute = require('./src/routes/team');
-const compRoute = require('./src/routes/competition');
-const playerRoute = require('./src/routes/player');
-const matchRoute = require('./src/routes/match');
-const searchRoute = require('./src/routes/search');
 const maintenanceRoute = require('./src/routes/maintenance');
 const schema = require('./src/routes/schema');
 
@@ -29,16 +23,10 @@ app.use(bp.urlencoded({ extended: true }));
 
 app.use('/images', express.static("images"));
 app.use('/maintenance', maintenanceRoute);
-app.use('/api/v2/auth', authRoute);
-app.use('/api/v2/team', teamRoute);
-app.use('/api/v2/competition', compRoute);
-app.use('/api/v2/player', playerRoute);
-app.use('/api/v2/match', matchRoute);
-app.use('/api/v2/search', searchRoute);
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV == 'development'
+    graphiql: process.env.NODE_ENV == 'dev'
 }));
 
 mongoose.set('strictQuery', false);
