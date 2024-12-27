dae368a680daa8472348ead5069972e81be66d0f
Eunoia <ezemaemmanuel6@gmail.com>
2024-12-27 07:17:34 +0100

    Removed route controllers for REST requests

--- src/routes/match.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { getAllMatches, getMatchPicks, getMatchDetails } = require('../controllers/match');
-
-router.get('/:matchId', getMatchDetails);
-router.get('/', getAllMatches);
-router.get('/prediction/outcomes', getMatchPicks);
-
-module.exports = router
--- src/routes/team.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { getAllTeams, getTeamDetails, getTeamMatches, getTeamPlayers } = require('../controllers/team')
-
-router.get('/', getAllTeams);
-router.get('/:teamId', getTeamDetails);
-router.get("/:teamId/matches", getTeamMatches);
-router.get("/:teamId/squad", getTeamPlayers);
-
-module.exports = router
